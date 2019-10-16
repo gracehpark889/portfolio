5ca790740edb30a95248acccb5ca9682a02ef79e
Grace H Park <gracehpark889@gmail.com>
2019-10-15 17:28:28 -0700

    update

--- js/hopeforpaws.js
+++ js/hopeforpaws.js
@@ -20,3 +20,13 @@ $(window).scroll(function() {
         $(e).addClass("anim");
    }
 });
+$(window).scroll(function() {
+    var e = ".hfp_swot",
+        hT = $(e).offset().top,
+        hH = $(e).outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS > (hT+hH-wH)-400){
+        $(e).addClass("anim");
+   }
+});
